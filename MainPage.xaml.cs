d1e2a6014a8db08c82e55b6daddd110d84690d37
Jonas Heilemann <134715426+JonHeil@users.noreply.github.com>
2024-07-24 16:42:51 +0200

    Der erste Schritt
    Erster Commit
    - Anfang die Homepage zu erstellen

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SportSyncSuggestions
+{
+    public partial class MainPage : ContentPage
+    {
+        double startX = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            TileStackLayout.GestureRecognizers.Add(panGesture);
+        }
+
+        private void OnPanUpdated(object? sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    startX = e.TotalX;
+                    break;
+                case GestureStatus.Running:
+                    double deltaX = startX - e.TotalX;
+                    TileScrollView.ScrollToAsync(TileScrollView.ScrollX + deltaX, 0, false);
+                    startX = e.TotalX;
+                    break;
+            }
+        }
+    }
+}
